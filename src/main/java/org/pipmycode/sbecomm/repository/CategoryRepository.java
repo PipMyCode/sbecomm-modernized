8a7bf613fdd1ac3673603654b5f75f0deacfd11a
PipMyCode <adigunmarcus@gmail.com>
2026-06-03 08:23:10 -0400

    Remove `AppConfig` and update `CategoryRepository` to use `Optional` for `findByCategoryName` and add `existsByCategoryName` method

--- src/main/java/org/pipmycode/sbecomm/repository/CategoryRepository.java
+++ src/main/java/org/pipmycode/sbecomm/repository/CategoryRepository.java
@@ -5,7 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByCategoryName(String categoryName);
+
+    Optional<Category> findByCategoryName(String categoryName);
+
+    boolean existsByCategoryName(String categoryName);
+
 
 }
