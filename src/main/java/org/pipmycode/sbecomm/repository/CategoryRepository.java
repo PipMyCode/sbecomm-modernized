4b361ea59043cb00cccf51c2d924676e925d3d1f
PipMyCode <adigunmarcus@gmail.com>
2026-06-11 18:43:05 +0100

    Annotate `CategoryRepository` with `@Repository` for consistency with Spring's repository pattern

--- src/main/java/org/pipmycode/sbecomm/repository/CategoryRepository.java
+++ src/main/java/org/pipmycode/sbecomm/repository/CategoryRepository.java
@@ -4,9 +4,11 @@ import org.pipmycode.sbecomm.model.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Optional<Category> findByCategoryName(String categoryName);
