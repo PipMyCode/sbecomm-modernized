8dbde6fe4920674504a12e69882691c886b79f98
PipMyCode <adigunmarcus@gmail.com>
2026-06-11 18:43:59 +0100

    Enhance `ResourceNotFoundException`: add fields for `resourceName`, `fieldName`, and `fieldValue` with a formatted message and annotate with `@Getter`

--- src/main/java/org/pipmycode/sbecomm/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/pipmycode/sbecomm/exceptions/ResourceNotFoundException.java
@@ -1,8 +1,20 @@
 package org.pipmycode.sbecomm.exceptions;
 
+import lombok.Getter;
+
+@Getter
 public class ResourceNotFoundException extends RuntimeException {
 
-    public ResourceNotFoundException(String message) {
-        super(message);
+    private final String resourceName;
+    private final String fieldName;
+    private final Long fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue) {
+
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
     }
 }
