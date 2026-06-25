54725f482cbe09071b1b954e2b7121d3c1e8c549
PipMyCode <adigunmarcus@gmail.com>
2026-06-25 21:42:59 +0100

    Fix RedisCacheManager missing bean

--- src/main/java/com/sbecomm/modernized/common/config/RedisConfig.java
+++ src/main/java/com/sbecomm/modernized/common/config/RedisConfig.java
@@ -4,8 +4,11 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
 import java.time.Duration;
 
 @Configuration
@@ -13,10 +16,15 @@ import java.time.Duration;
 public class RedisConfig {
 
     @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(60)) // Cache products for 60 minutes
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
                 .disableCachingNullValues()
+                // Use the Jackson serializer here instead of the CacheManager
                 .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }
